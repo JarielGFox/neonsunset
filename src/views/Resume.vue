daba322b817bd3a938abf443477bad9468510b36
Giovanni Crudele <crudelegio@gmail.com>
2023-07-12 13:53:14 +0200

    button styles

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -22,13 +22,13 @@ export default {
             <h1 class="display-5 fw-bolder mb-0"><span class="text-light">Competenze</span></h1>
         </div>
         <div class="row gx-5 justify-content-center">
-            <div class="col-lg-11 col-xl-9 col-xxl-8">
+            <div class="col-lg-12 col-xl-12 col-xxl-12">
                 <!-- Work Section-->
                 <section>
                     <div class="d-flex align-items-center justify-content-between mb-4">
                         <ResumeTitle class="mb-0" title="Esperienze" />
                         <!-- Link al cv-->
-                        <a class="btn btn-primary px-4 py-3"
+                        <a class="btn btn-cv btn-cta fs-6"
                             href="https://www.canva.com/design/DAE94LaqBAQ/NkxIeJRMhdnLVBF7h952dA/view?utm_content=DAE94LaqBAQ&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
                             target="_blank">
                             <div class="d-inline-block bi bi-download me-2"></div>
@@ -49,4 +49,10 @@ export default {
     </div>
 </template>
 
-<style></style>
+<style scoped>
+.btn-cv {
+    width: fit-content;
+    word-wrap: nowrap;
+    padding: 14px 14px;
+}
+</style>
