a4a0de472426b0df20eab9631ffffcfacb387b78
JarielGFox <crudelegio@gmail.com>
2023-08-25 18:00:53 +0200

    solved issue with pathing

--- src/php/includes/includes.php
+++ src/php/includes/includes.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
-require 'C:\Users\Giovanni\Documents\Coding\Portfolio\Vue.js\neonsunset\vendor\autoload.php';
-require 'C:\Users\Giovanni\Documents\Coding\Portfolio\Vue.js\neonsunset\vendor\phpmailer\phpmailer\src\Exception.php';
-require 'C:\Users\Giovanni\Documents\Coding\Portfolio\Vue.js\neonsunset\vendor\phpmailer\phpmailer\src\PHPMailer.php';
-require 'C:\Users\Giovanni\Documents\Coding\Portfolio\Vue.js\neonsunset\vendor\phpmailer\phpmailer\src\SMTP.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
