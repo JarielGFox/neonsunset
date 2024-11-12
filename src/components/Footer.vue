b246a4aed4ebcee690bc227c68f10e6a58eb9f23
JarielGFox <crudelegio@gmail.com>
2024-11-12 11:56:25 +0100

    updated personal portfolio

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,11 @@
 <script>
+ export default {
+    data() {
+        return {
+            currentYear: new Date().getFullYear(),
+        }
+    }
+ }
 </script>
 
 <template>
@@ -6,7 +13,7 @@
         <div class="container px-5">
             <div class="row align-items-center justify-content-between flex-column flex-sm-row">
                 <div class="col-auto">
-                    <div class="text-pink-light m-0"><em>Copyright &copy; Giovanni Crudele 2024</em></div>
+                    <div class="text-pink-light m-0"><em>Copyright &copy; Giovanni Crudele {{currentYear}} </em></div>
                 </div>
                 <div class="col-auto text-pink">
                     <span class="nav-item" href="#!">Privacy</span>
--- src/components/resume/Work.vue
+++ src/components/resume/Work.vue
@@ -10,14 +10,42 @@
                 <div class="row align-items-center gx-5">
                     <div class="col text-justify fs-5 mb-4 mb-lg-0">
                         <div class="bg-light p-4 rounded-4">
-                            <div class="text-primary fw-bolder mb-2">Apr 2024 - Presente</div>
+                            <div class="text-primary fw-bolder mb-2">Lug 2024 - Presente</div>
+                            <div class="small fw-bolder">Junior Front End Web Developer</div>
+                            <div class="small text-muted">• <a href="https://www.stantup.it/" title="Stantup">Stantup Service</a> |
+                                Hybrid</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 fs-5">
+                        <div class="mb-2">• <strong>Sviluppo front end JS con utilizzo di libreria jQuery</strong>:
+                            interfaccia grafica e dinamicità delle pagine su applicazioni lato web multipiattaforma.
+                        </div>
+                        <div class="mb-2">• <strong>Sviluppo backend con linguaggio PHP</strong>:
+                           implementazione delle best-practice di sviluppo su sicurezza e controlli lato backend, su gestione di dati tramite DB relazionale <strong>MySQL</strong>.
+                        </div>
+                        <div class="mb-2">• <strong>Bugfixing e migliore tecniche</strong>: identificazione precisa
+                            di errori tecnici o strutturali all'interno di codice legacy, seguita da
+                            miglioramenti alla logica e approccio di problem solving.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card shadow border-0 rounded-4 mb-5">
+            <div class="card-body p-5">
+                <div class="row align-items-center gx-5">
+                    <div class="col text-justify fs-5 mb-4 mb-lg-0">
+                        <div class="bg-light p-4 rounded-4">
+                            <div class="text-primary fw-bolder mb-2">Apr 2024 - Lug 2024</div>
                             <div class="small fw-bolder">Junior Front End Web Developer</div>
                             <div class="small text-muted">• FoxByte Srl | Full remote</div>
                         </div>
                     </div>
                     <div class="col-lg-8 fs-5">
                         <div class="mb-2">• <strong>Sviluppo front end con React JS e framework NextJS</strong>:
-                            Partecipazione attiva nel processo di sviluppo di un'applicazione web lato front-end tramite
+                            Partecipazione attiva nel processo di sviluppo di un'applicazione web lato front-end
+                            tramite
                             utilizzo di libreria React JS e framework NextJS.
                         </div>
                         <div class="mb-2">• <strong>Contributo regolare su progetto in collaborazione</strong>:
@@ -25,7 +53,8 @@
                             in un ambiente di lavoro apportando contributi significativi ad un progetto
                             condiviso.
                         </div>
-                        <div class="mb-2">• <strong>Bugfixing e migliore tecniche</strong>: identificazione precisa di
+                        <div class="mb-2">• <strong>Bugfixing e migliore tecniche</strong>: identificazione precisa
+                            di
                             errori tecnici all'interno del codice e della struttura del progetto, seguita da
                             miglioramenti alla logica del codice e
                             all'utilizzo di un'azione risolutiva tramite approccio di problem solving.
@@ -48,18 +77,22 @@
                     </div>
                     <div class="col-lg-8 fs-5">
                         <div class="mb-2">• <strong>Progettazione e Sviluppo di Landing Page</strong>: creazione di
-                            landing page eleganti e responsive, con un focus particolare sull'ottimizzazione UX/UI per
-                            massimizzare l'engagement degli utenti. Utilizzo di tecnologie all'avanguardia per garantire
+                            landing page eleganti e responsive, con un focus particolare sull'ottimizzazione UX/UI
+                            per
+                            massimizzare l'engagement degli utenti. Utilizzo di tecnologie all'avanguardia per
+                            garantire
                             prestazioni veloci e un'esperienza utente memorabile.
                         </div>
-                        <div class="mb-2">• <strong>Collaborazione e Innovazione in Team</strong>: partecipazione attiva
+                        <div class="mb-2">• <strong>Collaborazione e Innovazione in Team</strong>: partecipazione
+                            attiva
                             in un ambiente di lavoro collaborativo, apportando contributi significativi a progetti
                             condivisi. Promozione di un approccio orientato alla soluzione che ha potenziato
                             l'efficienza e la qualità del lavoro di squadra.
                         </div>
                         <div class="mb-2">• <strong>Analisi e Risoluzione Bug</strong>: Abile nell'identificazione
                             rapida e precisa di bug, seguita da un'azione risolutiva tramite problem solving. Questa
-                            competenza ha contribuito a mantenere elevate le performance dei progetti e a garantire la
+                            competenza ha contribuito a mantenere elevate le performance dei progetti e a garantire
+                            la
                             soddisfazione dei clienti.
                         </div>
                     </div>
